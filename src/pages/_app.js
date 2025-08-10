07dc217e9513a48a4435185d99fc2f6ca4290949
Shirish Shakya <84584836+Shirish-creator@users.noreply.github.com>
2025-08-10 00:06:14 -0400

    showcase project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,7 @@ import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
 import Link from "next/link";
 import { useState } from "react";
+import Head from "next/head";
 
 // Dynamically import the Threed component to ensure it renders only on the client side
 const Threed = dynamic(() => import("@/components/threedworld"), { ssr: false });
@@ -27,6 +28,13 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <link
+          href="https://api.fontshare.com/v2/css?f[]=clash-grotesk@200,300,400,500,600,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      
       {/* Persistent 3D Background */}
         <Threed buttonTrigger={buttonTrigger} />
       
@@ -40,11 +48,11 @@ export default function App({ Component, pageProps }) {
       <section className="text-white absolute h-full  w-full " style={{zIndex:1,width:'100%'}}> 
         <div className=" mx-auto  flex flex-col justify-end h-screen  max-w-5xl" > 
           <div className="max-w-lg mx-6 mb-32 xl:mb-44">
-          <p className="text:md xl:text-lg" style={{color:'#1276aa'}}>Client:  <span className="font-semibold">StudioGusto</span></p>
-
-            <h1 className=" mt-4 text-4xl lg:text-6xl text-red-400">Boom&Bloom</h1>
+                    <p className="text:md xl:text-lg" style={{color:'#1276aa'}}>Client:  <span className="font-semibold">Luminary Optics</span></p>
+  
+            <h1 className=" mt-4 text-4xl lg:text-6xl text-red-400">Capture Vision</h1>
             <p className="mt-2 text-md " style={{color:'#1276aa'}}>Role: UX/ UI/ Development/ 3D Design</p>
-            <p className="mt-5 text-lg lg:text-xl font-medium text-blue-900" style={{color:'#1276aa'}}>In an era of digital evolution, Boom & Bloom merges as a catalyst for innovation, a haven for boundles creativity.</p>
+            <p className="mt-5 text-lg lg:text-xl font-medium text-blue-900" style={{color:'#1276aa'}}>Revolutionizing the way photographers and videographers capture moments with cutting-edge camera technology and intuitive design.</p>
               <button onPointerDown={buttonTriggerFunction} onMouseEnter={buttonTriggerFunction} onClick={triggerProject} style={{pointerEvents:'all',boxShadow:'0 0 40px 5px rgba(228,103,103,.6)'}} className="px-6 py-3 mt-8 bg-red-400 text-lg">View Project</button>
   
      </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -24,11 +24,7 @@ export default function Home() {
     
   
     <section className="w-100 h-fit flex   relative px-44" >
-   <div className="text-white flex flex-row  gap-8 py-6" style={{pointerEvents:'all'}}>
-     
-      <p className="text-white">Company Logo</p>
-      <Link href={'projectone'}> Project One</Link>
-   </div>
+  
     </section>
     
 
